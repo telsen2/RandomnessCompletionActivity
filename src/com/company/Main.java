4d62323104ca30c2031194cc9aebe4af70c27112
GBTC441002UR <GBTC441002UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-06-04 12:43:44 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+import java.util.Random;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        int dienumber;
+        // create instance of Random class
+        Random rnd = new Random();
+
+        // Generate random integers in range 0 to 6
+
+                dienumber =rnd.nextInt(6);
+
+                        System.out.println("Your die roll was : " + dienumber);
+    }
+}
+
+/*
+// A Java program to demonstrate random number generation
+// using java.util.Random;
+import java.util.Random;
+
+public class generateRandom{
+
+    public static void main(String args[])
+    {
+        // create instance of Random class
+        Random rand = new Random();
+
+        // Generate random integers in range 0 to 999
+        int rand_int1 = rand.nextInt(1000);
+        int rand_int2 = rand.nextInt(1000);
+
+        // Print random integers
+        System.out.println("Random Integers: "+rand_int1);
+        System.out.println("Random Integers: "+rand_int2);
+
+        // Generate Random doubles
+        double rand_dub1 = rand.nextDouble();
+        double rand_dub2 = rand.nextDouble();
+
+        // Print random doubles
+        System.out.println("Random Doubles: "+rand_dub1);
+        System.out.println("Random Doubles: "+rand_dub2);
+    }
+}
+ */
